bcfe20b36e46f4ca2a3a588e997e97b183a69c5b
a <a@192.168.0.11>
2017-06-29 00:12:01 +0530

    initial setup

---
+++ GoodsServices/src/main/java/pdfBox/GoodsServices/App.java
@@ -0,0 +1,13 @@
+package pdfBox.GoodsServices;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
